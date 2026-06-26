bbaef16ae60029a18d13449fc6730bbb31650b42
leopoldo <leopoldoalvaradoantonio@gmail.com>
2026-06-26 13:05:38 -0600

    este es la clase de camioneta cs

---
+++ GestonDeCobroEstacionamiento/Camioneta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestonDeCobroEstacionamiento
+{
+    public class Camioneta : Vehiculo
+    {
+        public int NumeroEjes { get; set; }  
+
+        public Camioneta(int horas, int numeroEjes) : base(horas)
+        {
+            NumeroEjes = numeroEjes;
+        }
+
+        public override double CalcularTarifa()
+        {
+            return Horas * 45;
+        }
+    }
+}
