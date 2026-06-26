a2410a4c3bcf2690620b358c9518efb6678434b7
Aida Sujey Carrillo Hernández <aidasujeycarrillohernandez1@gmail.com>
2026-06-26 13:03:30 -0600

    Hice la clase automovil

---
+++ GestonDeCobroEstacionamiento/Automovil.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestonDeCobroEstacionamiento 
+{
+    public class Automovil : Vehiculo
+    {
+        public string Placa { get; set; }
+
+        public Automovil(int horas, string placa) : base(horas)
+        {
+            Placa = placa;
+        }
+
+        public override double CalcularTarifa()
+        {
+            return HorasEstancia * 30;
+        }
+    }
+
+
+
+
+}
+
