d0b4ee4d24d698c6ba9c97b6fd8b3bf2774ca3ba
juniorH <ch7435557@gmail.com>
2026-06-26 13:02:50 -0600

    Este es el apartado de Motocicletas

---
+++ GestonDeCobroEstacionamiento/Motocicleta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestonDeCobroEstacionamiento
+{
+    class Motocicleta : Veiculo
+    {
+        public int Cilindrada { get; set; }
+        public Motocicleta(int horas, int cilindrada) : base(horas)
+        {
+            Cilindrada = cilindrada;
+        }
+
+        public override double CalcularTrifa()
+        {
+            return Horas * 15;
+        }
+    }
+}
